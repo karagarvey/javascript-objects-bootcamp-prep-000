f3cf083447c6740bb10fd65b45862efba6d2e6d8
karagarvey <kgarvey87@gmail.com>
2018-03-14 00:26:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 var playlist = new Object({'artistName':'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+  return Object.assign(playlist, {[artistName]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName) {
